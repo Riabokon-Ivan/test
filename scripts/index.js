852b2b4b49d10a8c5d740309ebc0bd816c95b7b9
Peaceful Warrior <peaceful11111111@gmail.com>
2019-12-09 11:57:37 +0200

    change count of symbols

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const textarea = document.querySelector('#textarea');
 textarea.addEventListener('keyup', function countChar() {
   const len = textarea.value.length;
-  if (len >= 350) {
-    textarea.value = textarea.value.substring(0, 350);
+  if (len >= 300) {
+    textarea.value = textarea.value.substring(0, 300);
   } else {
     const div = document.querySelector('#charNum');
-    div.innerText = `${(350 - len)} / 350`;
+    div.innerText = `${(300 - len)} / 300`;
   }
 }
 );
